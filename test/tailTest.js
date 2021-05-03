f90790f3363f26be7b7f68371be580768a4240d1
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-05-03 21:16:58 +0000

    mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,11 @@
-const assertEqual = require('../assertEqual')
+// const assertEqual = require('../assertEqual')
+// const tail = require('../tail')
+// assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), 3);
+
+const assert = require('chai');
 const tail = require('../tail')
-
-
-
-
-assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), 3);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), 'Lighthouse', 'Labs');
+  });
+});
