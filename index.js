0b556e8cbe86a2572fbc916698d74c5d84f65183
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-05-03 22:18:34 +0000

    index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const  countOnly = require('./countOnly');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+
+module.exports = {
+  head:  head,
+  tail: tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  map: map,
+takeUntil: takeUntil
+}
